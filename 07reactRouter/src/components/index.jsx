499f2128541947257ab3e737f99cc56bf294eaad
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-01-15 03:37:17 +0530

    Done with react router dom

--- 07reactRouter/src/components/index.jsx
+++ 07reactRouter/src/components/index.jsx
@@ -3,5 +3,6 @@ import Header from "./header/Header";
 import Home from "./home/Home";
 import About from "./about/About";
 import Contact from "./contactUs/Contact";
+import User from "./user/User";
 
-export {Footer , Header , About , Contact , Home}
+export {Footer , Header , About , Contact , Home , User}
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,32 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
-import {Home , About , Contact } from './components'
+import {Home , About , Contact , User} from './components'
+import { Route } from 'react-router-dom'
 
 
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <Layout/> ,
-    children : [
-      {
-        path:"",
-        element : <Home/> 
-      },
-      {
-        path:"contact",
-        element : <Contact/> 
-      },
-      {
-        path:"about",
-        element : <About/> 
-      },
+// const router = createBrowserRouter([
+//   {
+//     path : "/",
+//     element : <Layout/> ,
+//     children : [
+//       {
+//         path:"",
+//         element : <Home/> 
+//       },
+//       {
+//         path:"contact",
+//         element : <Contact/> 
+//       },
+//       {
+//         path:"about",
+//         element : <About/> 
+//       },
       
-    ]
-  }
-])
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<Layout/>} >
+      <Route path = "" element = {<Home/>} />
+      <Route path = "about" element = {<About/>} />
+      <Route path = "conatct" element = {<Contact/>} />
+      <Route path = "user:id" element = {<User/>} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
