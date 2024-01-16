6b3c502a1e3d1e14454987dedfa4226ef324d3ea
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-01-16 18:16:36 +0530

    completed the theme toggler

--- 09themetoggler/src/App.jsx
+++ 09themetoggler/src/App.jsx
@@ -1,33 +1,34 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './Context/theme'
+import ThemeBtn from './Components/ThemeBtn'
+import Card from './Components/Card'
 
 function App() {
   const [themeMode , setThemeMode] = useState("light")
   
   const lightTheme = () => {
-     setThemeMode = "light"
+     setThemeMode("light")
   }
   const darkTheme = () => {
-     setThemeMode = "dark"
+    setThemeMode("dark")
   }
 
-  useEffect(()=>{
-    let htmlElement =  document.querySelector('html')
-    htmlElement.classList.remove("light" , "dark")
-    htmlElement.classList.add(themeMode)
-
-  } , [themeMode])
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
   return (
     <ThemeProvider value={{themeMode , lightTheme , darkTheme}}>
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-             {/* theme button*/}
+             <ThemeBtn/>
                     </div>
 
         <div className="w-full max-w-sm mx-auto">
-             {/* card */}
+             <Card/>
                 </div>
                       </div>
    </div>  
