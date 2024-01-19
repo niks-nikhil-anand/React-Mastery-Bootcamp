6b42e5d95870713989db1c7ab3a2a0dec7fcd3fa
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-01-20 00:24:49 +0530

    uses the useSelector and useDispatch

--- 11redusToolkitToDo/src/components/AddTodo.jsx
+++ 11redusToolkitToDo/src/components/AddTodo.jsx
@@ -11,6 +11,7 @@ const AddTodo = () => {
 
     const addTodoHandler = (e) => {
         dispatch(addTodo(input))
+        setInput('')
     }
 
     return (
--- 11redusToolkitToDo/src/components/Todo.jsx
+++ 11redusToolkitToDo/src/components/Todo.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { UseSelector , useDispatch, useSelector } from 'react-redux'
+import { removeTodo } from '../features/todo/todoslice'
+
 
 const todo = () => {
+    const todos = useSelector(state => state.todos )
+    const dispatch = useDispatch()
   return (
-    <div>todo</div>
+   <>
+   <div>Todos</div>
+   {todos.map((todo) => (
+    <li key={todo.id}>
+        {todo.text}
+        <button
+        onClick={()=> dispatch(removeTodo(todo.id))}
+        > ❌ 
+        </button>
+    </li>
+   ))}
+   </>
   )
 }
 
